ba9ee10407bc0760ec2ffd78719b0cbb817e5fae
congdv <congdaovan94@gmail.com>
2018-04-07 14:03:34 +0700

    scroll smooth

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-$('.nav-item a').on('click',function(){
+/*$('.nav-item a').on('click',function(){
     $(this).parent().parent().find('a.active').removeClass('active');
     $(this).addClass('active');
 
+})*/
+
+$(document).ready(function(){
+    var scrollLink = $('.scroll');
+    // Smooth scrolling
+    scrollLink.click(function(e){
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        },1000);
+
+    })
+
+    // Active link switching
+    /*
+    $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+        //console.log(scrollbarLocation);
+        scrollLink.each(function(){
+            var sectionOffset = $(this.hash).offset().top ;
+            if( sectionOffset < scrollbarLocation){
+                $(this).parent().parent().find('a.active').removeClass('active');
+                $(this).addClass('active');
+            }
+        })
+    })*/
 })
-
-function regular_map(){
-    var var_location = new google.maps.LatLng(16.464620,107.574690);
-
-    var var_mapoptions = {
-        center: var_location,
-        zoom:14
-    };
-
-    var var_map = new google.maps.Map(document.getElementById("map-container"),var_mapoptions);
-
-    var var_marker = new google.maps.Marker({
-        position: var_location,
-        map: var_map,
-        title:"Thành Phố Huế"
-    });
-}
-/*Initialize map*/
-google.maps.event.addDomListener(window,'load',regular_map);
