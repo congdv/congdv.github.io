5e94983d48ea7cc723ce86716c99a0b3ead6acd9
Cong Dao <congdaovan94@gmail.com>
2020-09-06 13:27:29 -0400

    Add App.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendfile(__dirname + '/index.html');
+})
+app.listen(port, () => console.log(`The application is running on port ${port}`))
